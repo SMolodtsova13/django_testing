46dc3a9e837ec2cf7a2eac7e00c1b4bee4978e2a
Светлана Молодцова <molodtsova201@yandex.ru>
2024-06-30 11:12:56 +0300

    refactoring

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -2,55 +2,52 @@ from http import HTTPStatus
 
 import pytest
 from pytest_django.asserts import assertRedirects
-
+from django.test.client import Client
 
 pytestmark = pytest.mark.django_db
 
 
+HOME_URL = pytest.lazy_fixture('home_url')
+LOGIN_URL = pytest.lazy_fixture('users_login_url')
+LOGOUT_URL = pytest.lazy_fixture('users_logout_url')
+SINGUP_URL = pytest.lazy_fixture('users_singnup_url')
+DETAIL_URL = pytest.lazy_fixture('news_detail_url')
+EDIT_URL = pytest.lazy_fixture('news_edit_url')
+DELETE_URL = pytest.lazy_fixture('news_delete_url')
+NOT_AUTHOR_CLIENT = pytest.lazy_fixture('not_author_client')
+AUTHOR_CLIENT = pytest.lazy_fixture('author_client')
+CLIENT_NOT_AUTHORIZATION = Client()
+
+
 @pytest.mark.parametrize(
-    'name, expected_status',
-    ((pytest.lazy_fixture('home_url')),
-     (pytest.lazy_fixture('users_login_url')),
-     (pytest.lazy_fixture('users_logout_url')),
-     (pytest.lazy_fixture('users_singnup_url')),
-     (pytest.lazy_fixture('news_detail_url'),)
-     )
+    'name, client_test, expected_status',
+    ((HOME_URL, CLIENT_NOT_AUTHORIZATION, HTTPStatus.OK),
+     (LOGIN_URL, CLIENT_NOT_AUTHORIZATION, HTTPStatus.OK),
+     (LOGOUT_URL, CLIENT_NOT_AUTHORIZATION, HTTPStatus.OK),
+     (SINGUP_URL, CLIENT_NOT_AUTHORIZATION, HTTPStatus.OK),
+     (DETAIL_URL, CLIENT_NOT_AUTHORIZATION, HTTPStatus.OK),
+     (DELETE_URL, NOT_AUTHOR_CLIENT, HTTPStatus.NOT_FOUND),
+     (EDIT_URL, NOT_AUTHOR_CLIENT, HTTPStatus.NOT_FOUND),
+     (EDIT_URL, AUTHOR_CLIENT, HTTPStatus.OK),
+     (DELETE_URL, AUTHOR_CLIENT, HTTPStatus.OK))
 )
-def test_pages_availability_for_anonymous_user(client, name,):
+def test_pages_availability_for_different_user(client_test,
+                                               name,
+                                               expected_status):
     """
     Главная страница, регистрация, входа, выхода, доступны пользователям.
     Страница отдельной новости доступна анонимному пользователю.
-    """
-    response = client.get(name)
-    assert response.status_code == HTTPStatus.OK
-
-
-@pytest.mark.parametrize(
-    'parametrized_client, expected_status',
-    ((pytest.lazy_fixture('author_client'), HTTPStatus.OK),
-     (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),),
-)
-@pytest.mark.parametrize(
-    'name',
-    (pytest.lazy_fixture('news_delete_url'),
-     pytest.lazy_fixture('news_edit_url')),
-)
-def test_pages_availability_for_different_users(parametrized_client,
-                                                name,
-                                                expected_status):
-    """
     Страницы удаления и редактирования доступны автору комментария.
     Авторизованный пользователь не может зайти на страницы
     редактирования или удаления чужих комментариев (ошибка 404).
     """
-    response = parametrized_client.get(name)
+    response = client_test.get(name)
     assert response.status_code == expected_status
 
 
 @pytest.mark.parametrize(
     'name',
-    (pytest.lazy_fixture('news_delete_url'),
-     pytest.lazy_fixture('news_edit_url')),
+    (DELETE_URL, EDIT_URL),
 )
 def test_redirect_for_anonymous_client(client, name, users_login_url):
     """Анонимный пользователь перенаправляется на страницу авторизации."""
